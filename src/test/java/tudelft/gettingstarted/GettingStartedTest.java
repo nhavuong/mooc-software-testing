ee27f56be1d1d7abe6d285aa4a7a6df5953e34b5
Nha Vuong <46470817+nhavuong@users.noreply.github.com>
2020-04-12 01:44:46 -0700

    Complete the test class for addfive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,27 +1,48 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
+	
+	GettingStarted g;
+	
+	@BeforeEach
+	public void setup() {
+		g = new GettingStarted();
+	}
 
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = g.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
-
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = g.addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+    
+    @Disabled
+    @Test
+    public void addFiveToMinus20() {
+        int result = g.addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+    
+    @Disabled
+    @Test
+    public void addFiveTo99() {
+    	int result = g.addFive(99);
+    	Assertions.assertEquals(104, result);
+    }
+    
+    @Test
+    public void addFiveToMinus1() {
+    	int result = g.addFive(-1);
+    	Assertions.assertEquals(4, result);
+    }
 }
